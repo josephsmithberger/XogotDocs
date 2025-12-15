2a69f737dd835512833ea48c1277f0edd458780a
Nicolas St-Amour <nstamour.dev@outlook.com>
2025-12-14 20:37:36 -0500

    Add step-by-step GDScript code files and update tutorial
    Introduced individual GDScript files for each step of the 'Your First Script' tutorial and updated the tutorial to reference these files using @Code blocks. This improves clarity and maintainability by separating code examples into dedicated files and aligning the tutorial steps with the corresponding code.

---
+++ Documentation.docc/Resources/code-files/step-by-step/your-first-script/scripting_04.gd
@@ -0,0 +1,4 @@
+extends Sprite2D
+
+var speed = 400
+var angular_speed = PI
---
+++ Documentation.docc/Resources/code-files/step-by-step/your-first-script/scripting_05.gd
@@ -0,0 +1,7 @@
+extends Sprite2D
+
+var speed = 400
+var angular_speed = PI
+
+func _process(delta):
+    rotation += angular_speed * delta
